94ac1552e53c25e54d273e0a8dd0436752a36bc8
davidalves1 <alves.david@outlook.com>
2017-05-06 15:50:44 -0300

    Incluir mensagem para a rota principal da api

--- app/routes/events.js
+++ app/routes/events.js
@@ -1,32 +1,13 @@
 module.exports = (app, db) => {
+
+	app.get('/v1', (req, res) => {
+		return res.json({message: "Seja bem vindo à API para consulta dos eventos da Paróquia São Pedro de Baixo Guandu! s2"});
+	})
+	
 	/**
-	 * The Create resource method
-	 * @param  '/api/events' The route to call this method
+	 * The Read a specific group of resources
+	 * @param  '/v1/events/:date' :date = YYYY-mm-dd
 	 */
-	app.post('/v1/events', (req, res) => {
-
-		const date_time = req.body.date || undefined;
-		const description = req.body.description || undefined;
-
-		if (date_time === undefined || description === undefined)
-			return res.status(422).json({error: 'Data e descrição são obrigatórias'});
-
-		const event = {
-			date: new Date(date_time),
-			description: description
-		};
-
-		db.collection('eventos').insert(event, (err, result) => {
-			if (err) {
-				console.log(err);
-				return res.status(500).json({error: `An error has occured.`});
-			}
-				
-			return res.json({message: 'Evento adicionado com sucesso'});
-
-		});
-	});
-
 	app.get('/v1/events/:date', (req, res) => {
 
 		const date = req.params.date || undefined;
@@ -54,4 +35,31 @@ module.exports = (app, db) => {
 			});
 	});
 
+	/**
+	 * The Create resource method
+	 * @param  '/api/events' The route to call this method
+	 */
+	app.post('/v1/events', (req, res) => {
+
+		const date_time = req.body.date || undefined;
+		const description = req.body.description || undefined;
+
+		if (date_time === undefined || description === undefined)
+			return res.status(422).json({error: 'Data e descrição são obrigatórias'});
+
+		const event = {
+			date: new Date(date_time),
+			description: description
+		};
+
+		db.collection('eventos').insert(event, (err, result) => {
+			if (err) {
+				console.log(err);
+				return res.status(500).json({error: `An error has occured.`});
+			}
+				
+			return res.json({message: 'Evento adicionado com sucesso'});
+
+		});
+	});
 };
